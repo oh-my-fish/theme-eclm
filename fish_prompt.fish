d32ca78a6223f882f19f022aa60bd3ccf4abaa46
Justin Hileman <justin@justinhileman.info>
2019-01-06 11:33:45 -0800

    Make stderr redirection compatible with Fish 3.0.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,10 +1,10 @@
 # name: eclm
 function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
 function fish_prompt
